0c4fed52af013ef9928e6e9f1764f7296f5b6cfc
Ichwan Rizqi Agung <emailkamu@gmail.com>
2026-04-23 14:51:21 +0700

    update navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <!-- Navbar Sederhana -->
 <nav class="navbar navbar-dark bg-primary shadow-sm mb-4">
     <div class="container-fluid px-2">
-        <span class="navbar-brand mb-0 h1">Ichwan Rizqi Agung</span>
+        <span class="navbar-brand mb-0 h1">Web Profile Ichwan-Dev</span>
     </div>
 </nav>
