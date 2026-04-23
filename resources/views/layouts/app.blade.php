e551a23df8327f8b695485ba49ea645d145e9b96
Ichwan Rizqi Agung <emailkamu@gmail.com>
2026-04-23 14:41:09 +0700

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Profil Curriculum Vitae Online. Berisi informasi data diri, keahlian, dan portofolio profesional.">
-    <title>Praktikum-1: CV Pribadi</title>
+    <title>Ichwan CV</title>
     
     <!-- Bootstrap 5.3 CSS via CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
